0457cf4ffacd846a4bb67270f75b4c80a4bd22c2
Apple Juice <mallari23adrian@gmail.com>
2023-08-06 21:13:18 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          position: "absolute",
+          top: 0,
+          left: 0,
+        }}
+      ></div>
+    </>
+  );
+}
+
+export default App;
